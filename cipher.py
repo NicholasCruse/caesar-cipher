c1083445d0299370f23fb6b8cb3be5573c63ee30
NicholasCruse <ncruselex@gmail.com>
2024-11-17 13:21:12 -0500

    Cipher.py has been edited and update with my code

--- cipher.py
+++ cipher.py
@@ -1,1 +1,27 @@
-# add your code here
+def encrypt_text(Message,Rshift):
+    ans = ""
+    # iterate over the given Message
+    for Letter in range(len(Message)):
+        char = Message[Letter]
+        # check if the character is a letter.
+        if char.isalpha():
+            # check if space is there then simply add space.
+            if char==" ":
+                ans+=" "
+            # check if a character is uppercase then encrypt it accordingly.
+            elif (char.isupper()):
+                ans += chr((ord(char) + Rshift-65) % 26 + 65)
+            # check if a character is lowercase then encrypt it accordingly.
+            else:
+                ans += chr((ord(char) + Rshift-97) % 26 + 97)
+        #Keeps Non-alphabetic characters unchange or erased.
+        else:
+            ans += char
+    
+    return ans
+
+Message = input("Input your Message to be encrypted.")
+Rshift = 5
+print("The Message Text is: " + Message)
+print("The pattern is Shifted over to the right by: " + str(Rshift))
+print("The encrypted Text is: " + encrypt_text(Message,Rshift))
